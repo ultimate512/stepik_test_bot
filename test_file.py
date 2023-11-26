e264b5ac44559731ccced78c9fc6e16784d15e3b
Ivan Putintsev <ultimate512@gmail.com>
2023-11-26 19:08:09 +0300

    created Users class

--- test_file.py
+++ test_file.py
@@ -1,3 +1,14 @@
-print('test')
-print('test2')
-print('test3')
+from dataclasses import dataclass
+
+
+@dataclass
+class Users:
+    User_id: int
+
+    def get_user_id(self):
+        return f'User id is {self.User_id}'
+
+
+if __name__ == "__main__":
+    user1 = Users(142)
+    print(user1.get_user_id())
